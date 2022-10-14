79ee7f7c62bcfc95e69457d4ccd1c56daa927c15
dai re <dairewahandi@gmail.com>
2022-10-14 14:03:31 +0700

    Testing responsive

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#000000",
+      },
+      fontFamily: {
+        pop: "pop",
+      },
+    },
+  },
+  plugins: [],
+};
